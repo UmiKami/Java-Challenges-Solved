9b1861dafed8b403f7228c56164451058c217af3
UmiKami <ggoku02@gmail.com>
2021-05-08 14:24:23 -0400

    Formatted

--- EvenDigitSum/src/EvenDigitSum.java
+++ EvenDigitSum/src/EvenDigitSum.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class EvenDigitSum {
+public class EvenDigitSum {
+    public static void main(String[] args) {
+        System.out.println(getEvenDigitSum(-252));
+    }
+
+    public static int getEvenDigitSum(int number){
+        if (number < 0){
+            return -1;
+        }else {
+            int sum = 0;
+            int num = number;
+            while (num > 0){
+                int lastDigit = num % 10;
+                num /= 10;
+
+                if (lastDigit % 2 == 0){
+                    sum += lastDigit;
+                }
+            }
+            return sum;
+        }
+    }
 }
