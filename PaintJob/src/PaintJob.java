ce982dab2d936d45dcb0c3685bc94365ec7cf618
UmiKami <ggoku02@gmail.com>
2021-05-17 11:40:32 -0400

    Control Flow Exercises Done

---
+++ PaintJob/src/PaintJob.java
@@ -0,0 +1,31 @@
+public class PaintJob {
+    public static void main(String[] args) {
+        System.out.println(getBucketCount(3.4,2.1,1.5,2));
+        System.out.println(getBucketCount(8.45,4.6,2.25));
+        System.out.println(getBucketCount(6.26,2.2));
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets){
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0){
+            return -1;
+        }else{
+            return (int)(Math.ceil((width * height) / areaPerBucket) - extraBuckets);
+        }
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket){
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0){
+            return -1;
+        }else{
+            return (int)(Math.ceil((width * height) / areaPerBucket));
+        }
+    }
+
+    public static int getBucketCount(double area, double areaPerBucket){
+        if ( area <= 0 || areaPerBucket <= 0){
+            return -1;
+        }else{
+            return (int)(Math.ceil(area / areaPerBucket));
+        }
+    }
+}
